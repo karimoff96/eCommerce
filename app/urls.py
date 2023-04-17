903cde3fe59eca374040bd79164cc7517b9857af
dkarimoff <doniyorkarimoff96gmail.com>
2023-04-17 11:26:16 +0500

    wishlist+orders pages

--- app/urls.py
+++ app/urls.py
@@ -9,32 +9,45 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('category/<slug:val>', views.CategoryView.as_view(), name='category'),
-    path('category_title/<val>', views.CategoryTitle.as_view(), name='category_title'),
-    path('product_detail/<int:pk>', views.ProductDetail.as_view(), name='product_detail'),
+    path('category_title/<val>',
+         views.CategoryTitle.as_view(), name='category_title'),
+    path('product_detail/<int:pk>',
+         views.ProductDetail.as_view(), name='product_detail'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('address/', views.address, name='address'),
-    path('updateAddress/<int:pk>', views.updateAddress.as_view(), name='updateAddress'),
+    path('updateAddress/<int:pk>',
+         views.updateAddress.as_view(), name='updateAddress'),
+    path('delete_address/<int:pk>', views.delete_address, name='delete_address'),
 
     path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
     path('cart/', views.show_cart, name='showcart'),
     path('checkout/', views.checkout.as_view(), name='checkout'),
+    path('paymentdone/', views.payment_done, name='paymentdone'),
+    path('orders/', views.orders, name='orders'),
 
     path('pluscart/', views.plus_cart),
     path('minuscart/', views.minus_cart),
     path('removecart/', views.remove_cart),
 
 
-    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
-    path('login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
-    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name = 'app/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name='passwordchange'),
-    path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
+    path('registration/', views.CustomerRegistrationView.as_view(),
+         name='customerregistration'),
+    path('login/', auth_view.LoginView.as_view(template_name='app/login.html',
+         authentication_form=LoginForm), name='login'),
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html',
+         form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name='passwordchange'),
+    path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(
+        template_name='app/passwordchangedone.html'), name='passwordchangedone'),
     path('logout/', auth_view.LogoutView.as_view(next_page='login'), name='logout'),
 
-    path('password_reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
-    path('password_reset/done/', auth_view.PasswordChangeDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html',
+         form_class=MyPasswordResetForm), name='password_reset'),
+    path('password_reset/done/', auth_view.PasswordChangeDoneView.as_view(
+        template_name='app/password_reset_done.html'), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html',
          form_class=MySetPasswordForm), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_view.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'),
+    path('password_reset_complete/', auth_view.PasswordResetCompleteView.as_view(
+        template_name='app/password_reset_complete.html'), name='password_reset_complete'),
 
 
 
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.db.models import Count
 from django.views import View
-from . models import Product, Customer, Cart
+from . models import Product, Customer, Cart, Payment, OrderPlaced, Wishlist
 from . forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
 from django.db.models import Q
@@ -10,19 +10,31 @@ from django.db.models import Q
 
 
 def home(request):
-    return render(request, 'app/home.html')
+    totalitem = 0
+    if request.user.is_authenticated:
+        totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/home.html', locals())
 
 
 def about(request):
-    return render(request, 'app/about.html')
+    totalitem = 0
+    if request.user.is_authenticated:
+        totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/about.html', locals())
 
 
 def contact(request):
-    return render(request, 'app/contact.html')
+    totalitem = 0
+    if request.user.is_authenticated:
+        totalitem = len(Cart.objects.filter(user=request.user))
+    return render(request, 'app/contact.html', locals())
 
 
 class CategoryView(View):
     def get(self, request, val):
+        totalitem = 0
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
         product = Product.objects.filter(category=val)
         title = Product.objects.filter(category=val).values('title')
         return render(request, 'app/category.html', locals())
@@ -31,6 +43,9 @@ class CategoryView(View):
 class CategoryTitle(View):
     def get(self, request, val):
         product = Product.objects.filter(title=val)
+        totalitem = 0
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
         title = Product.objects.filter(
             category=product[0].category).values('title')
         return render(request, 'app/category.html', locals())
@@ -39,12 +54,19 @@ class CategoryTitle(View):
 class ProductDetail(View):
     def get(self, request, pk):
         product = Product.objects.get(pk=pk)
+        wishlist = Wishlist.objects.filter(Q(product=product)& Q(user=request.user))
+        totalitem = 0
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
         return render(request, 'app/productdetail.html', locals())
 
 
 class CustomerRegistrationView(View):
     def get(self, request):
         form = CustomerRegistrationForm()
+        totalitem = 0
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
         return render(request, 'app/customerregistration.html', locals())
 
     def post(self, request):
@@ -61,6 +83,9 @@ class CustomerRegistrationView(View):
 class ProfileView(View):
     def get(self, request):
         form = CustomerProfileForm()
+        totalitem = 0
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
         return render(request, 'app/profile.html', locals())
 
     def post(self, request):
@@ -85,13 +110,24 @@ class ProfileView(View):
 
 def address(request):
     add = Customer.objects.filter(user=request.user)
+    totalitem = 0
+    if request.user.is_authenticated:
+        totalitem = len(Cart.objects.filter(user=request.user))
     return render(request, 'app/address.html', locals())
 
+
+def delete_address(request, pk):
+    adress = Customer.objects.get(pk=pk)
+    adress.delete()
+    return redirect('address')
 
 class updateAddress(View):
     def get(self, request, pk):
         add = Customer.objects.get(pk=pk)
         form = CustomerProfileForm(instance=add)
+        totalitem = 0
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
         return render(request, 'app/updateAddress.html', locals())
 
     def post(self, request, pk):
@@ -117,7 +153,7 @@ def add_to_cart(request):
     product_id = request.GET.get('prod_id')
     product = Product.objects.get(id=product_id)
     cart = Cart.objects.filter(user=user, product_id=product_id)
-    if len(cart)!=0:
+    if len(cart) != 0:
         cart[0].quantity += 1
         cart[0].save()
     else:
@@ -133,11 +169,17 @@ def show_cart(request):
         value = p.quantity*p.product.discounted_price
         amount = amount+value
     totalamount = amount+40
+    totalitem = 0
+    if request.user.is_authenticated:
+        totalitem = len(Cart.objects.filter(user=request.user))
     return render(request, 'app/addtocart.html', locals())
 
 
 class checkout(View):
     def get(self, request):
+        totalitem = 0
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
         user = request.user
         add = Customer.objects.filter(user=user)
         cart_items = Cart.objects.filter(user=user)
@@ -146,7 +188,48 @@ class checkout(View):
             value = p.quantity * p.product.discounted_price
             famount = famount+value
         totalamount = famount+40
+        payamount = int(totalamount*100)
+        # client
+        data = {'amount': payamount, 'currency': "USD",
+                'receipt': 'order_rcptid_12'}
+        # pament_response = client.order.create(data=data)
+        # order_id = payment_response['id']
+        # order_status = payment_response['status']
+        # if order_status == 'create':
+        #     payment = Payment(
+        #         user=user,
+        #         amount=totalamount,
+        #         order_id = order_id,
+        #         order_status=order_status,
+        #     )
+        #     payment.save()
         return render(request, 'app/checkout.html', locals())
+
+
+def payment_done(request):
+    # order_id = request.GET.get('order_id')
+    # payment_id = request.GET.get('payment_id')
+    # cust_id = request.GET.get('cust_id')
+    # user = request.user
+    # customer = Customer.objects.get(id=cust_id)
+    # payment = Payment.objects.get(razorpay_order_id=order_id)
+    # payment.paid = True
+    # payment.razorpay_payment_id = payment_id
+    # payment.save()
+    # cart = Cart.objects.filter(user=user)
+    # for c in cart:
+    #     OrderPlaced(user=user, customer=customer, product=c.product,
+    #                 quantity=c.quantity, payment=payment).save()
+    #     c.detele()
+
+    return redirect('orders')
+
+def orders(request):
+    totalitem = 0
+    if request.user.is_authenticated:
+        totalitem = len(Cart.objects.filter(user=request.user))
+    order_placed = OrderPlaced.objects.filter(user=request.user)
+    return render(request, 'app/orders.html', locals())
 
 
 def plus_cart(request):
